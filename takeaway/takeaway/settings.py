1f5015b91a88364fce080111b5cd4a354ed3a848
lucasarge <sargentl@stu.otc.school.nz>
2025-05-09 10:52:02 +1200

    trying to runserver

--- takeaway/takeaway/settings.py
+++ takeaway/takeaway/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'takeaway.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
