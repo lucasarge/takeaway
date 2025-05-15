bd2c91c6f048a9f8ab1cb4de6af4223efedc54d6
lucasarge <sargentl@stu.otc.school.nz>
2025-05-16 10:57:09 +1200

    superuser added

--- takeaway/takeaway/urls.py
+++ takeaway/takeaway/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('', views.index),
     path('about/', views.about),
     path('user/', include('user.urls'))
