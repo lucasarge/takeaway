bda858ee1202874f479a65608fd7532bceb57362
lucasarge <sargentl@stu.otc.school.nz>
2025-05-29 13:30:29 +1200

    gridded menu items.

--- takeaway/takeaway/wsgi.py
+++ takeaway/takeaway/wsgi.py
@@ -13,7 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'takeaway.settings')
 
-# There is no error in your wsgi.py file.
-# The error is in your URL or template configuration.
-# Make sure you are visiting /products/menu/ (not /products/menu.html/)
-# and that your template is named menu.html and is in the correct templates directory.
+application = get_wsgi_application()
