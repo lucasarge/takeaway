d932374bb03abad0f2a504325a447fd6d1a11039
luca_sarge <sargentl@stu.otc.school.nz>
2025-05-31 10:46:08 +1200

    added very basic page + structure for slugs

--- takeaway/products/urls.py
+++ takeaway/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
-    path('', views.menu_view),
+    path('', views.products_menu, name="menu"),
+    path('<slug:slug>', views.product_page, name="page")
 ]
--- takeaway/products/views.py
+++ takeaway/products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Product
+from django.http import HttpResponse
+
 
 # Create your views here.
-def menu_view(request):
+def products_menu(request):
     products = Product.objects.all()
     return render(request, 'products/menu.html', {'products': products})
+
+def product_page(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, 'products/product.html', {'product': product})
