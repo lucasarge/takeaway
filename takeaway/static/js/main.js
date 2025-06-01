775f1fbbc661285a9f6fd860818e330725602c8d
luca_sarge <sargentl@stu.otc.school.nz>
2025-06-01 18:24:59 +1200

    added modal for quicksearch, about to do nav

--- takeaway/static/js/main.js
+++ takeaway/static/js/main.js
@@ -1,1 +1,17 @@
 console.log('This is JS from your About')
+
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+var openBtn = document.getElementsByClassName("openBtn")[0];
+var modal = document.getElementById("quicksearch-modal");
+
+console.log("test")
+
+openBtn.onclick = function() {
+    modal.style.display = "block";
+    openBtn.style.display = "none";
+}
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+    openBtn.style.display = "inline-block";
+}
