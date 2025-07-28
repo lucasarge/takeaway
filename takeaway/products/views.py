5deb455761dd1c64d6795ab655ccd4391c1f3863
lucasarge <sargentl@stu.otc.school.nz>
2025-07-28 15:08:40 +1200

    having issues with git

--- takeaway/products/views.py
+++ takeaway/products/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render, redirect
-from .models import Product, Cart, CartItem
+from .models import Product, Cart, CartItem, Label, LabelProduct
 from django.http import HttpResponse, JsonResponse
 import json
 from django.contrib import messages
+from django.db.models import Q
 
 
 # Create your views here.
 def products_menu(request):
-    products = Product.objects.all()
-    return render(request, 'products/menu.html', {'products': products})
+    search_query=request.GET.get('search','')
+    labels = Label.objects.all()
+    if search_query=='':
+        products = Product.objects.all()
+    else:
+        products = Product.objects.filter(
+            Q(name__icontains = search_query) |
+            Q(labelproduct__label__name__icontains = search_query)
+            ).distinct()
+    
+    return render(request, 'products/menu.html', {'products': products, 
+                                                  'labels':labels,
+                                                  'search_query':search_query})
 
 def product_page(request, slug):
     product = Product.objects.get(slug=slug)
-    return render(request, 'products/product.html', {'product': product})
+    labels = product.labels.all()
+    cart, created = Cart.objects.get_or_create(user=request.user, completed=False)
+    cartitem = cart.cartitems.filter(product=product).first()
+    return render(request, 'products/product.html', {'product': product, 
+                                                     'labels':labels,
+                                                     'item':cartitem})
+    
 
 def cart(request):
 
