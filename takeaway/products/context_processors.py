64a3423f628d98f3faf0eef34fd5748be4bea058
lucasarge <sargentl@stu.otc.school.nz>
2025-07-28 14:22:05 +1200

    ingredients and doc next

---
+++ takeaway/products/context_processors.py
@@ -0,0 +1,7 @@
+from .models import Cart
+
+def cart_item_quantity(request):
+    if request.user.is_authenticated:
+        cart, created = Cart.objects.get_or_create(user=request.user, completed=False)
+        return {'cart_item_quantity':cart.num_of_items}
+    return{'cart_item_quantity':0}
--- takeaway/takeaway/views.py
+++ takeaway/takeaway/views.py
@@ -1,9 +1,12 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
+from products.models import Cart
 
 def index(request):
     #return HttpResponse("Hello World!")
     return render(request, 'index.html')
+
+
 
 def about(request):
     #return HttpResponse("About page.")
